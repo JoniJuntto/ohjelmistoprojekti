e36cb5476f666495c2277642f17e4db96d58d0e5
Joni Juntto <bgm043@myy.haaga-helia.fi>
2021-04-13 12:19:43 +0300

    Radiobuttongroup working

---
+++ fromsapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RadioButtonsGroup from './components/RadioButtonsGroup';
+
+function App() {
+
+  return(
+    <div>
+      <RadioButtonsGroup />
+    </div>
+  );
+}
+export default App;
---
+++ fromsapp/src/components/RadioButtonsGroup.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import { Button } from '@material-ui/core';
+
+export default function RadioButtonsGroup(props) {
+
+  const [value, setValue] = useState('esimerkki 1');
+  const [FetchData, setFetchData] = useState(null);
+  
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  }
+    
+  const listItems = FetchData.map((fetchData) =>
+    <li>{fetchData.kysymys}</li>
+  );
+
+  const sendButtonPress = async () =>{
+    const url = 'https://formsappi.herokuapp.com/api';
+
+    try {
+        const response = await fetch(url);
+        var data = await response.json();
+        setFetchData(data);
+        console.log(data)
+    }catch (error){
+        console.log(error);
+    }
+  }
+
+  return (
+      <div>
+    <FormControl component="fieldset">
+        <FormLabel component="legend">Otsikko</FormLabel>
+        <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+            <FormControlLabel value="esimerkki 1" control={<Radio />} label="esimerkki 1" />
+            <FormControlLabel value="esimerkki 2" control={<Radio />} label="esimerkki 2" />
+            <FormControlLabel value="esimerkki 3" control={<Radio />} label="esimerkki 3" />
+        </RadioGroup>
+    </FormControl>
+    <Button variant='contained' onClick={sendButtonPress}>Lähetä</Button>
+
+    <div>
+            <ul>{listItems}</ul>
+        </div>
+
+    </div>
+  );
+}
